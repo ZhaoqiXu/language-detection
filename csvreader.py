0af9deb4732b690858bda762107a47a7cb400874
qinc1026 <qccanada2013@gmail.com>
2017-10-06 20:29:18 -0400

    add csvreader file
    char statistics of each language is done

---
+++ csvreader.py
@@ -0,0 +1,25 @@
+import csv
+import collections
+
+trainfile1 = "dat_train_x.csv"
+trainfile2 = "dat_train_y.csv"
+
+with open(trainfile1,'rb') as f1,open(trainfile2,'rb') as f2:
+    xreader = list(csv.reader(f1))
+    yreader = list(csv.reader(f2))
+    CounterList = []
+    for i in range(0,5):
+         CounterList.append(collections.Counter())
+    for index,x in enumerate(xreader[1:]):
+        #first value of csv column is "Category" so it should be skipped.
+        x = x[1]
+        counter = collections.Counter(x)
+        y = yreader[index+1][1]
+        y = int(y)
+        CounterList[y] = CounterList[y] + counter
+    CounterList2 = []
+    for counter in CounterList:
+        totalchar = sum(counter.values())
+        CounterList2.append([(i, float(counter[i]) / float(totalchar)) for i in counter])
+    for counter in CounterList2:
+        print counter
